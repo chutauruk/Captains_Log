c4571310f195dfd971963907e79ac4379fe612d0
chutauruk <cyn.uruk@gmail.com>
2022-12-19 19:05:38 -0700

    new route setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+
+//Show Route
+
+//New Route
+app.get("/logs/new", (req, res) => {
+    res.render("New");
+  });
+
+
+//Create Route
+
+//Edit Route
---
+++ views/New.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+const myStyle = {
+  color: "#3B7177",
+  backgroundColor: "#F1AAB7",
+  padding: "5px",
+  fontFamily: "Helvetica",
+  textAlign: "center",
+};
+const bodyOne = {
+  color: "#3B7177",
+  backgroundColor: "#F1AAB7",
+  padding: "5px",
+  fontFamily: "Helvetica",
+  textAlign: "center",
+};
+
+export default class New extends Component {
+  render() {
+    return (
+      <div>
+        <h1 style={myStyle}> New Pokemon</h1>
+        <body style={bodyOne}></body>
+        <form action="/logs" method="post">
+          Title: <input type="text" name="title" /> <br />
+          Entry: <input type="textarea" name="entry" /> <br />
+          Is Ship Broken?: <input type="checkbox" name="shipIsBroken" />
+          <br />
+          <input type="submit" name="" value="Create Ship Entry" />
+        </form>
+        <a href="/pokemon">Return without editing</a>
+      </div>
+    );
+  }
+}
+
